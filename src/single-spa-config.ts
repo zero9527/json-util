38ac3e8addaa262eeb9c215a7be1babe1b674fea
zero9527 <zero9527>
2020-06-05 14:29:11 +0800

    fix: 打包部署后，不加载子应用的问题

--- src/single-spa-config.ts
+++ src/single-spa-config.ts
@@ -3,7 +3,9 @@ import { registerApplication, start } from 'single-spa';
 registerApplication({
   name: '@vue-mf/calendar',
   app: () => (window as any).System.import('@vue-mf/calendar'),
-  activeWhen: '/#/calendar',
+  activeWhen: location => {
+    return location.href.includes('/calendar');
+  },
   customProps: {
     root: 'json-util',
   },
